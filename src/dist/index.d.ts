77abcbb2f4a4bea13f3065f288be383cd27003ab
dr4g00n <dr4g00n@users.noreply.github.com>
2026-04-03 11:24:12 +0800

    ENEN-time-20260403

---
+++ src/dist/index.d.ts
@@ -0,0 +1,4 @@
+declare const _default: import("openclaw/plugin-sdk/plugin-entry").PluginEntryConfig & {
+    default: import("openclaw/plugin-sdk/plugin-entry").PluginEntryConfig;
+};
+export default _default;
